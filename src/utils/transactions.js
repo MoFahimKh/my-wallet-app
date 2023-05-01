e9da18e175e4d7d3dee0236d9a543a7b1e1259f0
MfahimKh <mkhan@qodeleaf.com>
2023-05-01 20:49:08 +0530

    fix network change issue in transactions.js utility file

--- src/utils/transactions.js
+++ src/utils/transactions.js
@@ -1,16 +1,17 @@
 import { ethers } from "ethers";
 import USDT_ABI from "./usdtAbi";
 
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner();
 export const sendEther = async (amount, receiversAddress) => {
   try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
     const transx = {
       to: receiversAddress,
       value: ethers.utils.parseEther(amount),
     };
     const signAndSend = await signer.sendTransaction(transx);
     signAndSend.wait();
+    window.location.reload();
   } catch (error) {
     console.log(error);
   }
@@ -18,6 +19,8 @@ export const sendEther = async (amount, receiversAddress) => {
 
 export const sendUSDT = async (amount, receiversAddress) => {
   try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
     const USDTAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
     const USDTContract = new ethers.Contract(USDTAddress, USDT_ABI, signer);
     const amountInWei = ethers.utils.parseEther(amount);
@@ -25,7 +28,7 @@ export const sendUSDT = async (amount, receiversAddress) => {
     const balance = await provider.getBalance(signerAdd);
     if (balance > 0) {
       const transx = await USDTContract.transfer(receiversAddress, amountInWei);
-      await transx.wait;
+      await transx.wait();
     } else {
       console.log("not enough usdt");
       alert("not enough usdt");
